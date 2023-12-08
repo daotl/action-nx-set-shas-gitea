4ea9d93f8ff59764dede8b21392e786d735e870c
zhaoweijie <zwjCheer@163.com>
2023-12-08 20:43:40 +0800

    fix: add log

--- find-successful-workflow.ts
+++ find-successful-workflow.ts
@@ -59,6 +59,8 @@ let BASE_SHA: string;
     }
   } else {
     const pushPayload = github.context.payload as PushEvent;
+    console.log("pushPayload:", pushPayload);
+
     console.log("pushPayload.commits: ", JSON.stringify(pushPayload.commits));
 
     try {
